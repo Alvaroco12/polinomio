351e3dd42616416a015512ab36da8e506da28a85
Álvaro <alvaro130303@gmail.com>
2025-04-02 12:25:44 +0200

    basico creado

---
+++ basico.py
@@ -0,0 +1,25 @@
+class Nodo:
+    def __init__(self, dato = None, siguiente = None):
+        self.dato = dato
+        self.siguiente = siguiente
+
+class DatoPolinomio:
+    def __init__(self, valor = 0, termino = 0):
+        self.valor = valor
+        self.termino = termino
+
+class Polinomio:
+    def __init__(self):
+        self.grado = None
+        self.temrinoMayor = None
+
+    def agregartermino(polinomio, valor, termino):
+        pass
+
+    def modificarTermino(polinomio, termino):
+        pass
+
+    def obtenerValor(polinomio, termino):
+        pass
+
+    
